73acefdd68e500d4e0e3e13b7085ea771e851ba3
lx <alexk@netefx.de>
2012-07-03 21:33:50 +0200

    let ss combine all css files instead of letting sass do it. In dev mode its still visible which css file is used for what. also boilerpate added

--- mysite/code/Pages/Page.php
+++ mysite/code/Pages/Page.php
@@ -45,21 +45,36 @@ class Page extends SiteTree {
 }
 
 class Page_Controller extends ContentController {
-	public static $allowed_actions = array();
-	public function init() {
-		parent::init();
-		Requirements::set_combined_files_folder(project() . '/_combinedfiles');
-		Requirements::combine_files('main.js', array(
+	
+    public static $allowed_actions = array();
+	
+    public function init() {
+		
+        parent::init();
+        
+        Requirements::set_combined_files_folder(project() . '/_combinedfiles');
+		
+        Requirements::combine_files('main.js', array(
 			THIRDPARTY_DIR . '/jquery/jquery.min.js',
-			// THIRDPARTY_DIR . '/jquery-ui/jquery-ui.min.js',
+         // THIRDPARTY_DIR . '/jquery-ui/jquery-ui.min.js',
 			THIRDPARTY_DIR . '/json-js/json2.js',
-			THIRDPARTY_DIR . '/jquery-entwine/dist/jquery.entwine-dist.js',
+		 // THIRDPARTY_DIR . '/jquery-entwine/dist/jquery.entwine-dist.js',
 			PROJECT_THIRDPARTY_DIR . '/fancybox/jquery.fancybox-1.3.4.pack.js',
-			PROJECT_THIRDPARTY_DIR . '/modernizr/modernizr-2.5.3.min.js',
+            PROJECT_THIRDPARTY_DIR . '/modernizr/modernizr-2.5.3.min.js',
+		 // PROJECT_THIRDPARTY_DIR . '/bootstrap/js/bootstrap.min.js',
 			project() . '/javascript/plugins.js',
 			project() . '/javascript/main.js',
 		));
-		Requirements::themedCSS('screen');
-		Requirements::css(PROJECT_THIRDPARTY_DIR . '/fancybox/jquery.fancybox-1.3.4.css');
+        
+        Requirements::combine_files('screen.css', array(
+            project() . '/css/h5bp.css',
+         // PROJECT_THIRDPARTY_DIR . '/bootstrap/css/bootstrap.min.css',
+         // PROJECT_THIRDPARTY_DIR . '/bootstrap/css/bootstrap-responsive.min.css',
+            project() . '/css/layout.css',
+            project() . '/css/typography.css',
+            project() . '/css/form.css',
+            project() . '/css/ie.css',
+            PROJECT_THIRDPARTY_DIR . '/fancybox/jquery.fancybox-1.3.4.css'
+        ));
 	}
 }
