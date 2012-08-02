978d937654ca3e2b7781d08ff53ce49be6935b8b
lx <alexk@netefx.de>
2012-08-02 16:25:09 +0200

    deleted all filters because they are not compatible with SS3. HasValueFilter should now be working with SS3

--- netefxTools/code/filters/HasValueFilter.php
+++ netefxTools/code/filters/HasValueFilter.php
@@ -1,33 +1,44 @@
 <?php
 /**
- * Selects content that has a value OR on the other Hand is NULL
+ * Selects content that has a value OR on the other Hand is NULL or ''
+ * @version 0.2 (2012-08-02)
+ * 
+ * @package netefxTools
+ * @subpackage filters* 
+ * 
+ * @example $DescriptionField = new DropdownField ('HasDescrition',  'Has a descrition', array("" => "all", TRUE => "yes", FALSE => "no"));     
  */
 class HasValueFilter extends SearchFilter {
-	
-	/**
-	 * @return $query
-	 */
-	public function apply(SQLQuery $query) {
-		
-        $query = $this->applyRelation($query);
-		
-        if ($this->value) {
-            return $query->where(sprintf(
-                "%s IS NOT NULL",
-                $this->getDbName()
-            ));    
-        }
-        else {
-            return $query->where(sprintf(
-                "%s IS NULL",
-                $this->getDbName()
-            ));    
-        }
+    
+    public function apply(DataQuery $query) {
+        $this->model = $query->applyRelation($this->relation);
+        $where = array();
         
-	}
-	
-	public function isEmpty() {
+        if(is_array($this->getValue())) {
+            foreach($this->getValue() as $value) {
+                $value = Convert::raw2sql($value);
+                if ($value) {
+                    $where[] = sprintf("%s IS NOT NULL AND %s <> ''", $this->getDbName(), $this->getDbName());    
+                }
+                else {
+                    $where[] = sprintf("%s IS NULL OR %s = ''", $this->getDbName(), $this->getDbName());   
+                }
+            }
+
+        } else {
+            $value = Convert::raw2sql($this->getValue()); 
+            if ($value) {
+                $where[] = sprintf("%s IS NOT NULL AND %s <> ''", $this->getDbName(), $this->getDbName());
+            }
+            else {
+                $where[] = sprintf("%s IS NULL OR %s = ''", $this->getDbName(), $this->getDbName());    
+            }
+        }
+
+        return $query->where(implode(' OR ', $where));
+    }
+    
+    public function isEmpty() {
         return $this->getValue() == null || $this->getValue() == '';
-	}
+    }
 }
-?>
