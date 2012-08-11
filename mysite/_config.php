dc91bd767f414b656352b1f9c833566e14a6bbe7
Zauberfisch <admin@zauberfisch.at>
2012-08-11 12:07:49 +0000

    MINOR: moved error settings up so error reporting is turned on before the rest of config MINOR: fixed locale string in lang files MINOR: removed not existing extension calls

--- mysite/_config.php
+++ mysite/_config.php
@@ -5,13 +5,33 @@ $project = 'mysite';
 // ****************************************************
 // **************** database & general settings *******
 // ****************************************************
-global $database;
-$database = 'silverstripe3-boilerplate';
 MySQLDatabase::set_connection_charset('utf8');
 // use the _ss_environment.php file for configuration
 require_once ('conf/ConfigureFromEnv.php');
 define('PROJECT_THIRDPARTY_DIR', project() . '/thirdparty');
 define('PROJECT_THIRDPARTY_PATH', project() . '/' . PROJECT_THIRDPARTY_DIR);
+
+// ****************************************************
+// **************** error & dev settings **************
+// ****************************************************
+// always log errors
+SS_Log::add_writer(new SS_LogFileWriter('../silverstripe.log'), SS_Log::ERR);
+if (!Director::isLive()) {
+	// turn on errors
+	ini_set('display_errors', 1);
+	error_reporting(E_ALL);
+	// display template filenames as comments in the html output
+	// SSViewer::set_source_file_comments(true);
+	// flush templates every reload, this may make your site slow,
+	// and might cause "filetime" errors, which appear when the template gets flushed twice in the same moment
+	SSViewer::flush_template_cache();
+} else {
+	// we are in live mode, send errors per email
+	SS_Log::add_writer(new SS_LogEmailWriter('myEmail@mysite.com'), SS_Log::ERR);
+	// turn of error reporting
+	ini_set('display_errors', 0);
+	error_reporting(0);
+}
 
 // ****************************************************
 // **************** i18n ******************************
@@ -44,8 +64,8 @@ Email::setAdminEmail('myEmail@mysite.com');
 // ****************************************************
 // **************** extensions ************************
 // ****************************************************
-Object::add_extension('SiteConfig', 'mysiteSiteConfig');
-Object::add_extension('SiteTree', 'mysiteSiteTree');
+//Object::add_extension('SiteConfig', 'mysiteSiteConfig');
+//Object::add_extension('SiteTree', 'mysiteSiteTree');
 
 // ****************************************************
 // **************** CMS *******************************
@@ -54,25 +74,3 @@ LeftAndMain::require_css('mysite/css/cms.css');
 // CMSMenu::remove_menu_item('SecurityAdmin');
 // CMSMenu::remove_menu_item('CMSSettingsController');
 // CMSMenu::remove_menu_item('Help'); // still not possible ?
-
-// ****************************************************
-// **************** error & dev settings **************
-// ****************************************************
-// always log errors
-SS_Log::add_writer(new SS_LogFileWriter('../silverstripe.log'), SS_Log::ERR);
-if (!Director::isLive()) {
-	// turn on errors
-	ini_set('display_errors', 1);
-	error_reporting(E_ALL);
-	// display template filenames as comments in the html output
-	// SSViewer::set_source_file_comments(true);
-	// flush templates every reload, this may make your site slow,
-	// and might cause "filetime" errors, which appear when the template gets flushed twice in the same moment
-	SSViewer::flush_template_cache();
-} else {
-	// we are in live mode, send errors per email
-	SS_Log::add_writer(new SS_LogEmailWriter('myEmail@mysite.com'), SS_Log::ERR);
-	// turn of error reporting
-	ini_set('display_errors', 0);
-	error_reporting(0);
-}
