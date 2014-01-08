aa4990dc6f9b315ca392ec2617682829cfaf9d2c
Zauberfisch <admin@zauberfisch.at>
2014-01-08 06:33:26 +0000

    compile css without line_comments per default, updated scss/compass config file

--- mysite/config.rb
+++ mysite/config.rb
@@ -1,13 +1,22 @@
-project_type = :stand_alone
+# compass / scss config file
+# see http://compass-style.org/help/tutorials/configuration-reference/ for documentation
 
-# Set this to the root of your project when deployed:
+# possible values: :development, :production
+environment = :development
+
+# source commends in the compiled css that indicate the scss line number, set to true to enable
+line_comments = false
+
+# possible values: :nested, :expanded, :compact, :compressed
+output_style = (environment == :production) ? :compressed : :expanded
+
+# define base path for compass and relevant folders
 http_path = "/"
 css_dir = "css"
 sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "javascript"
 fonts_dir = "fonts"
-output_style = :extended
 
-# To enable relative paths to assets via compass helper functions. Uncomment:
+# make asset urls relative
 relative_assets = true
