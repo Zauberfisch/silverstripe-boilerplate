40a54518840c3fb5544b067c43b22440a24993f4
Zauberfisch <admin@zauberfisch.at>
2014-01-22 15:38:10 +0000

    google analytics snippet (closes #7)

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -38,11 +38,23 @@ class Page_Controller extends ContentController {
 			project() . '/javascript/timer.js',
 			project() . '/javascript/main.js',
 		));
-		// we need to insert modernizr into <head> for html5shiv to work
+		// insert modernizr into <head> for html5shiv to work
 		Requirements::insertHeadTags(sprintf(
-			'<script type="text/javascript" src="%s"></script>',
+			'<script src="%s"></script>',
 			PROJECT_THIRDPARTY_DIR . '/modernizr/modernizr.min.js'
 		));
+		// insert google analytics into <head> to also track visitors that cancle the pageload before it completed
+		//Requirements::insertHeadTags(sprintf(
+		//	"<script>
+		//		(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+		//		function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+		//		e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+		//		e.src='//www.google-analytics.com/analytics.js';
+		//		r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+		//		ga('create','%s');ga('send','pageview');
+		//	</script>",
+		//	'UA-XXXXX-X'
+		//));
 		Requirements::combine_files('main.css', array(
 			PROJECT_THIRDPARTY_DIR . '/h5bp/normalize.css',
 			PROJECT_THIRDPARTY_DIR . '/h5bp/h5bp.css',
